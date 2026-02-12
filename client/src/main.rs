37079b637030d6529707ea882b792caa6d1e3609
hirokuma <hiro9ma@gmail.com>
2026-02-12 10:12:43 +0900

    check content-type

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,53 +1,50 @@
 use anyhow::Result;
 use std::io::{BufRead, BufReader};
 
-fn json_call(req: &str) -> Result<()> {
+fn rest_call(req: &str) -> Result<()> {
     let mut res = ureq::get(format!("http://localhost:3000/{}", req)).call()?;
     let status = res.status();
-    let body = res.body_mut().read_json::<serde_json::Value>()?;
-
-    if status == ureq::http::StatusCode::OK {
-        let json_str = serde_json::to_string_pretty(&body)?;
-        println!("{}", json_str);
-    } else {
+    if status != ureq::http::StatusCode::OK {
         let json_str: serde_json::Value = serde_json::json!({
             "status": serde_json::Value::String(status.to_string()),
-            "error": body,
         });
         let json_str = serde_json::to_string_pretty(&json_str)?;
         println!("{}", json_str);
+        return Ok(());
     }
-    Ok(())
-}
-
-fn stream_call(req: &str) -> Result<()> {
-    let mut res = ureq::get(format!("http://localhost:3000/{}", req)).call()?;
-    let status = res.status();
-
-    if status == ureq::http::StatusCode::OK {
-        let reader = BufReader::new(res.body_mut().as_reader());
+    let content_type = res
+        .headers()
+        .get("content-type")
+        .and_then(|h| h.to_str().ok())
+        .unwrap_or("")
+        .to_lowercase();
+    let body = res.body_mut();
+    if content_type.contains("application/x-ndjson") {
+        let reader = BufReader::new(body.as_reader());
         for line in reader.lines() {
             let line = line?;
-            if line.is_empty() { continue; }
-
+            if line.is_empty() {
+                continue;
+            }
             let json_val: serde_json::Value = serde_json::from_str(&line)?;
             let json_str = serde_json::to_string_pretty(&json_val)?;
             println!("{}", json_str);
         }
-    } else {
-        let json_str: serde_json::Value = serde_json::json!({
-            "status": serde_json::Value::String(status.to_string()),
-        });
-        let json_str = serde_json::to_string_pretty(&json_str)?;
+    } else if content_type.contains("application/json") {
+        let body = body.read_json::<serde_json::Value>()?;
+        let json_str = serde_json::to_string_pretty(&body)?;
         println!("{}", json_str);
+    } else {
+        let body = body.read_to_string()?;
+        println!("{}", body);
     }
     Ok(())
 }
 
 fn main() -> Result<()> {
     println!("-[json]-----------------------");
-    json_call("json")?;
+    rest_call("json")?;
     println!("-[stream]-----------------------");
-    stream_call("stream")?;
+    rest_call("stream")?;
     Ok(())
 }
